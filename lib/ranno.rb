9a8939c7b88ef51425172b684679f228c43e83da
Max Aller <nanodeath@gmail.com>
2009-01-11 17:31:20 -0800

    Lots more cleaning up and fixes, and avoided some .send stuff in favor of class_eval.

--- lib/ranno.rb
+++ lib/ranno.rb
@@ -4,42 +4,55 @@ require 'extlib'
 module Ranno
   module Annotations
     # Class annotations fire once, when the method itself is initialized
-    def class_annotation(method_name, *args, &block)
-      self.class.send(:define_method, method_name) do |*args|
-        add_current_annotations(:class, {:method => method_name, :args => args})
+    def class_annotation(method_name, &block)
+      self.class.class_eval do
+        define_method(method_name) do |*args|
+          add_current_annotations(:class, {
+              :method => method_name,
+              :args => args})
+        end
+        define_method((method_name.to_s + "_annotation").to_sym, block)
       end
-      self.class.send(:define_method, (method_name.to_s + "_annotation").to_sym, &block)
-#      class << self
-#        define_method((method_name.to_s + "_annotation").to_sym, block)
-#      end
     end
 
     # Instance annotations fire every time the method is called
-    def instance_annotation(method_name, *args, &block)
-      self.class.send(:define_method, method_name) do |*args2|
-        add_current_annotations(:instance, {:method => method_name, :args => args, :block => block})
+    def instance_annotation(method_name, definition_args={}, &block)
+      self.class.class_eval do
+        define_method(method_name) do |*args|
+          add_current_annotations(:instance, {
+              :method => method_name,
+              :definition_args => definition_args, 
+              :args => args,
+              :block => block})
+        end
       end
-      self.send(:define_method, (method_name.to_s + "_annotation").to_sym, &block)
+      self.class_eval do
+        define_method((method_name.to_s + "_annotation").to_sym, block)
+      end
     end
   end
 
   module Base
     include Extlib::Hook
 
-    @@current_annotations = {}
+    @@annotations_for_next_method = {}
     def get_current_annotations
-      @@current_annotations
+      @@annotations_for_next_method
     end
 
     def add_current_annotations(type, value)
-      (@@current_annotations[type] ||= []).push(value)
+      (@@annotations_for_next_method[type] ||= []).push(value)
     end
 
-    def params=(ann)
+    def reset_annotations_for_next_method
+      @@annotations_for_next_method = {}
+    end
+
+    def ranno_params=(ann)
       @@current_args = ann
     end
 
-    def params
+    def ranno_params
       @@current_args
     end
     
@@ -63,59 +76,46 @@ module Ranno
         self.send((ann[:method].to_s + '_annotation').to_sym, method, *ann[:args])
       end
       (get_current_annotations[:instance] || []).each do |ann|
+        #        puts "ann is #{ann[:args].inspect}"
         hook_before = hook_after = false
-        ann[:args].each do |arg|
-          if arg.is_a? Hash
-            if arg[:hook].is_a? Array
-              hook_before = true if arg[:hook].include? :before
-              hook_after = true if arg[:hook].include? :after
+        ann[:definition_args].each_pair do |key, value|
+          if key == :hook
+            if value.is_a? Array
+              hook_before = true if value.include? :before
+              hook_after = true if value.include? :after
             else
-              hook_before = true if arg[:hook] == :before
-              hook_after = true if arg[:hook] == :after
+              hook_before = true if value == :before
+              hook_after = true if value == :after
+              hook_before = hook_after = true if value == :both
             end
           end
         end
+
         if hook_before
           before(method) do
-            self.params = {:method => method, :args => ann[:args].reject {|key| key == :after}}
+            tmp_params = ann[:definition_args].reject {|key, value| key == :hook}
+            tmp_params[:hook] = :before
+            self.ranno_params = tmp_params
             annotation_method = (ann[:method].to_s + '_annotation').to_sym
             self.send annotation_method, method, *ann[:args]
           end
         end
+
         if hook_after
           after(method) do
-            self.params = {:method => method, :args => ann[:args].reject {|key| key == :after}}
+            tmp_params = ann[:definition_args].reject {|key, value| key == :hook}
+            tmp_params[:hook] = :after
+            self.ranno_params = tmp_params
             annotation_method = (ann[:method].to_s + '_annotation').to_sym
             self.send annotation_method, method, *ann[:args]
           end
         end
       end
 
-      @@current_annotations = {}
+      reset_annotations_for_next_method
     end
 
     register_instance_hooks :my_method_added
-
-    #    def method_missing(sym, *args, &block)
-    #      #      method_name = sym.to_s
-    #      is_annotation = false
-    #      if ::Ranno::Annotations.is_class_annotation? sym
-    #        #        puts "found a class annotation: #{sym}"
-    #        ann = {:method => sym, :args => args}
-    #        ann[:block] = block if block_given?
-    #        (@@current_annotations[:class_annotation] ||= []) << ann
-    #        is_annotation = true
-    #      end
-    #      if ::Ranno::Annotations::Core.is_instance_annotation? sym
-    #        #        puts "found an instance annotation: #{sym}"
-    #        ann = {:method => sym, :args => args}
-    #        ann[:block] = block if block_given?
-    #        (@@current_annotations[:instance_annotation] ||= []) << ann
-    #        is_annotation = true
-    #      end
-    #      super unless is_annotation
-    #    end
-
 
     @@hooking = false
     def before_install_hook
--- spec/ranno_spec.rb
+++ spec/ranno_spec.rb
@@ -8,10 +8,31 @@ module ClassAnnotations
     @fie_list << method_name
   end
 
-  instance_annotation :counter, :hook => :after do |method_name, hook|
+  class_annotation :foo do |method_name|
+    @foo_list ||= []
+    @foo_list << method_name
+  end
+
+  instance_annotation :counter, :hook => :before do |method_name|
     @counter ||= {}
     @counter[method_name] = 0 unless @counter.key? method_name
     @counter[method_name] += 1
+  end
+
+  instance_annotation :timer, :hook => :both do |method_name, opts|
+    @timer ||= {}
+    @timer[method_name] ||= []
+    opts ||= {}
+    if(ranno_params[:hook] == :before)
+      @timer[method_name].push(Time.now)
+    else
+      time_taken = Time.now - @timer[method_name].pop
+      if (!opts[:ignore_recursion] || @timer[method_name].size == 0) && opts[:verbose]
+        puts "Time It: #{method_name} took #{time_taken}s"
+      end
+      @timer_result ||= {}
+      @timer_result[method_name] = time_taken
+    end
   end
 end
 
@@ -24,8 +45,17 @@ class TestClass1
     puts "hello"
   end
 
+  foo
+  foo
+  def say_greetings
+    puts "greetings"
+  end
+
   def self.retrieved_fied
     @fie_list
+  end
+  def self.retrieved_food
+    @foo_list
   end
 end
 
@@ -48,8 +78,50 @@ class TestClass2
     
   end
 
+  timer
+  def time_me
+    1.upto(1000) do
+      # nothing
+    end
+  end
+
+  @@fibo = {}
+
+  timer :ignore_recursion => true
+  def smart_fibonacci(n)
+    raise if n < 0
+    return @@fibo[n] if @@fibo.key? n
+    case n
+    when 0
+      @@fibo[n] = 0
+    when 1
+      @@fibo[n] = 1
+    else
+      @@fibo[n - 1] = smart_fibonacci(n - 1)
+      @@fibo[n - 2] = smart_fibonacci(n - 2)
+      @@fibo[n - 1] + @@fibo[n - 2]
+    end
+  end
+
+  timer :ignore_recursion => true
+  def dumb_fibonacci(n)
+    raise if n < 0
+    case n
+    when 0
+      0
+    when 1
+      1
+    else
+      dumb_fibonacci(n - 1) + dumb_fibonacci(n - 2)
+    end
+  end
+
+  def get_time_of sym
+    @timer_result[sym]
+  end
+
   def get_count_of sym
-#    puts "getting counter in #{self} (#{@counter})"
+    #    puts "getting counter in #{self} (#{@counter})"
     @counter[sym]
   end
 
@@ -72,8 +144,12 @@ describe "ranno" do
       TestClass1.retrieved_fied.should eql([:say_hello])
     end
 
-    it "should execute multiple methods with teh same class annotations" do
+    it "should execute multiple methods with the same class annotations" do
       TestClass2.retrieved_fied.should eql([:say_hello, :say_goodbye])
+    end
+
+    it "should allow duplicate annotations on the same method" do
+      TestClass1.retrieved_food.should eql([:say_greetings, :say_greetings])
     end
   end
 
@@ -82,6 +158,14 @@ describe "ranno" do
       t2 = TestClass2.new
       4.times { t2.count_me_in }
       t2.get_count_of(:count_me_in).should equal(4)
+
+      t2.time_me
+      t2.dumb_fibonacci(15)
+      t2.smart_fibonacci(15)
+      t2.get_time_of(:dumb_fibonacci).should satisfy {|n|
+        n > t2.get_time_of(:smart_fibonacci)
+      }
+
     end
   end
 end
